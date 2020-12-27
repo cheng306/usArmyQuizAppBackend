640d81c811785aeba77ad91fdcae4717a451d4e0
davidlam <yuensengd@gmail.com>
2020-12-26 22:38:02 -0800

    feat: allow cors request

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import * as bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import indexRouter from './server/routes/index';
 import questionsRouter from './server/routes/questions';
 import unitsRouter from './server/routes/units';
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false,
 }));
+app.use(cors());
 
 app.use(
   indexRouter,
