0f9df7a56e7edd50f1dfd838b7a75655ec9ef287
davidlam <yuensengd@gmail.com>
2020-12-26 17:58:24 -0800

    fix: updated ssh command

--- src/database/backupLog/backupLogService.ts
+++
@@ -1,9 +0,0 @@
-import { UnitType } from '../../utils/enums';
-
-export function getParentUnit(unitId: number, parentUnitType: UnitType) {
-  return `${unitId}${parentUnitType}`;
-}
-
-export function getUnits(unitType: UnitType, parentUnitId: number) {
-  return `${unitType}${parentUnitId}`;
-}
