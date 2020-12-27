37704b7e3a8be5d4bc870842c47163e5305e20bf
davidlam <yuensengd@gmail.com>
2020-12-27 12:20:36 -0800

    fix: change get req body to params

--- src/server/routes/questions.ts
+++ src/server/routes/questions.ts
@@ -5,11 +5,11 @@ import question from '../mock/question';
 const router = express.Router();
 
 // GET questions
-router.get('/questions', (req: Request, res: Response) => {
-  const { unitId, unitType, questionCounts }: GetQuestionsBody = req.body;
+router.get('/questions', (req: Request<unknown, unknown, unknown, GetQuestionsBody>, res: Response) => {
+  const { unitId, unitType, questionCounts }: GetQuestionsBody = req.query;
   if (!unitId || !unitType || !questionCounts) {
     res.status(404);
-    return res.send({ errorMessage: 'Incorrect request body.' });
+    return res.send({ errorMessage: 'Incorrect request params.' });
   }
   const questions: Question[] = [];
   for (let i = 0; i < questionCounts; i += 1) {
