78a74216282eae93d8aeec9e4a2f566385ee46e0
davidlam <yuensengd@gmail.com>
2020-12-26 21:36:54 -0800

    fix: removed database connection

--- src/database/sqlServer/db.ts
+++ src/database/sqlServer/db.ts
@@ -1,14 +1,14 @@
 import sql from 'mssql';
 import dbConfig from './dbConfig';
 
-const connectionPool = new sql.ConnectionPool(dbConfig, (res) => {
-  console.log(`res: ${res}`);
-});
+// const connectionPool = new sql.ConnectionPool(dbConfig, (res) => {
+//   console.log(`res: ${res}`);
+// });
+//
+// const db = connectionPool.connect().then(() => {
+//   console.log('success!');
+// }).catch((err) => {
+//   console.log(`err: ${err}`);
+// });
 
-const db = connectionPool.connect().then(() => {
-  console.log('success!');
-}).catch((err) => {
-  console.log(`err: ${err}`);
-});
-
-export { connectionPool, db };
+// export { connectionPool, db };
