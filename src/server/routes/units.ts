fdd8122dddd271b86a3cb3ba232a440c45785963
cheng306 <johnnycheng0306@gmail.com>
2021-01-01 23:07:34 -0500

    feat: api to deleted units

--- src/server/routes/units.ts
+++ src/server/routes/units.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
-import { getUnitsWithType } from '../services/dbManager';
-import { PostUnitsBody, PutUnitsBody, Unit } from '../../utils/apiTypes';
+import { deleteUnit, getUnitsWithType } from '../services/dbManager';
+import {
+  DeleteUnitsBody, PostUnitsBody, PutUnitsBody, Unit,
+} from '../../utils/apiTypes';
 import { UnitType } from '../../utils/enums';
 
 const router = express.Router();
@@ -11,9 +13,8 @@ router.get('/units', (req: Request<unknown, unknown, unknown, {id: number | unde
     return getUnitsWithType(new Set([UnitType.BATTALION, UnitType.BRIGADE, UnitType.DIVISION]))
       .then((units: Unit[]) => res.status(200).send({ units }))
       .catch((error) => res.status(404).send({ errorMessage: error.message }));
-  } else {
-    return res.status(200).send({ query: req.query });
   }
+  return res.status(200).send({ query: req.query });
 });
 
 router.get('/units/:unitType', (req: Request<{unitType:string}, unknown, unknown, {id:number}>, res: Response) => {
@@ -33,6 +34,16 @@ router.put('/units', (req: Request<PutUnitsBody>, res: Response) => {
   const { unitType, unitId, newName }: PutUnitsBody = req.body;
   console.log(unitType + unitId + newName);
   return res.status(200).send({ body: req.body });
+});
+
+router.delete('/units', (req: Request<DeleteUnitsBody>, res: Response) => {
+  const { unitType, unitId }: DeleteUnitsBody = req.body;
+  console.log(unitType + unitId);
+  return deleteUnit(unitId, unitType).then(
+    (success:boolean) => {
+      res.status(200).send({ Success: success });
+    },
+  ).catch((error) => res.status(404).send({ errorMessage: error.message }));
 });
 
 export default router;
